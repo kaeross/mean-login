a1f28a3faa73ae789d17a448986258f9775b78d7
kaeross <kaeross@icloud.com>
2019-08-18 21:59:45 +0100

    fix  http service

--- src/app/common.service.ts
+++ src/app/common.service.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json'
-  }),
-};
-
 export interface Config {
   newUser: boolean,
   passwordMatch: boolean,
@@ -18,11 +12,11 @@ export class CommonService {
 
   constructor(private http: HttpClient) {}
 
-  requestLogin(user: Object): Observable<HttpResponse<Config>>  {
+  requestLogin(user: Object)  {
     return this.http.post<Config>('http://localhost:8080/api/login', user, { observe: 'response' })
   }
 
-  requestRegister(user: Object): Observable<HttpResponse<Config>>  {
+  requestRegister(user: Object)  {
     return this.http.post<Config>('http://localhost:8080/api/register', user, { observe: 'response' })
   }
 }

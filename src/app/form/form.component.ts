6fb90e1c8bb4c42f94eff86ab385f304b77d4de4
kaeross <kaeross@icloud.com>
2019-08-18 22:10:26 +0100

    remove confirm password field

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -22,7 +22,6 @@ export class FormComponent {
   @Input() email: string;
   @Input() dOb: string;
   @Input() password: string;
-  @Input() confirmPassword: string;
 
   // Custom error messages
   errorMsg: object = {
@@ -34,11 +33,6 @@ export class FormComponent {
     const now:Date = new Date()
     const months:Array<string> = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
     return `${now.getFullYear()}-${months[now.getMonth()]}-${now.getDate()}`
-  }
-
-  // Check passwords match
-  get matchingPasswords() {
-    return this.password === this.confirmPassword
   }
 
   onSubmit(loginForm: NgForm) {
